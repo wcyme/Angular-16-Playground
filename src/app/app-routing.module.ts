56b8c552bc712749fa324daa156cd07e4824bbc1
johnny <wcy_me@hotmail.com>
2023-06-21 00:33:58 +0800

    [Add] Add routing of vessel list component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { VesselEditComponent } from './vessel-edit/vessel-edit.component';
+import { VesselListComponent } from './vessel-list/vessel-list.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'vessel/edit', component: VesselEditComponent}
-
+  { path: 'vessel/edit', component: VesselEditComponent},
+  { path: 'vessel/list', component: VesselListComponent},
 ];
 
 @NgModule({
